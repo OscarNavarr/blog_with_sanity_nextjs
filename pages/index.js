0ea9715870963a49d3569263c6522d075da9db3b
Oscar Alejandro <oscarnavarroaviles@gmail.com>
2023-10-30 16:54:42 +0100

    Sanity was added

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,31 @@
+
+import client_sanity from '../lib/client';
 import Head from 'next/head';
 import Layout, { siteTitle } from '../components/layout';
 import utilStyles from '../styles/utils.module.css';
+ 
 
-export default function Home() {
+import { getSortedPostsData } from '../lib/posts';
+
+export async function getServerSideProps() {
+  const allPostsData = getSortedPostsData();
+
+  const query = `*[_type == "publication"]`;
+
+  const publications = await client_sanity.fetch(query);
+
+  return {
+    props: {
+      allPostsData,
+      publications,
+    },
+  };
+}
+
+
+export default function Home({ allPostsData, publications }) {
+
+
   return (
     <Layout home>
       <Head>
@@ -10,11 +33,30 @@ export default function Home() {
       </Head>
       <section className={utilStyles.headingMd}>
         <p>[Your Self Introduction]</p>
+        
+        {
+          publications[0] ? <p>{publications[0].title}</p> : <p>no hay publicaciones</p>
+        }*
         <p>
           (This is a sample website - you’ll be building a site like this in{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
         </p>
       </section>
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              {title}
+              <br />
+              {id}
+              <br />
+              {date}
+            </li>
+          ))}
+        </ul>
+      </section>
     </Layout>
   );
 }
+
---
+++ sanity/publication.js
@@ -0,0 +1,22 @@
+export default {
+    name: 'publication',
+    title: 'Publication',
+    type: 'document',
+    fields: [
+        {
+            name: 'title',
+            title: 'Title',
+            type: 'string',
+        },
+        {
+            name: 'description',
+            title: 'Description',
+            type: 'string',
+        },
+        {
+            name: 'date',
+            title: 'Date',
+            type: 'date',
+        }
+    ]
+}
